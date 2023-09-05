ebf26c72c546cb87b913c6fa33999e89dad23a64
Aslan_Agadadashev <aslan.pressa@yandex.ru>
2023-09-05 13:25:34 +0300

    Add zadacha_21.py

---
+++ zadacha_21.py
@@ -0,0 +1,18 @@
+# Задача №21. Решение в группах
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001", " V ":" S009"},
+# {"VI": "S005"}, {"VII": " S005"}, {"V":" S009"}, {"VIII":" S007"}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+lst = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001", "V":"S009"},
+{"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
+
+print("Original list: ", lst)
+
+res_set = set()
+for cur_dict in lst:
+    for key in cur_dict:
+        res_set.add(cur_dict[key])
+print(res_set)
+    
