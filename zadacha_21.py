788d2699ae89072d302eefb49571bad9e77804a9
Aslan_Agadadashev <aslan.pressa@yandex.ru>
2023-09-05 13:42:43 +0300

    Add info zadacha_21.py

--- zadacha_21.py
+++ zadacha_21.py
@@ -9,10 +9,20 @@ lst = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001", "V":"S009"},
 {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
 
 print("Original list: ", lst)
+# Первый метод ======================
+# res_set = set()
+# for cur_dict in lst:
+#     for key in cur_dict:
+#         res_set.add(cur_dict[key])
+# print(res_set)
+# ====================================
 
+# Второй метод =======================
 res_set = set()
 for cur_dict in lst:
+    print(cur_dict.keys())
+    print(cur_dict.values())
+    print(cur_dict.items())
     for key in cur_dict:
         res_set.add(cur_dict[key])
-print(res_set)
-    
+print(*res_set, sep='\n')
