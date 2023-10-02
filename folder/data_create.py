5dfda290b089b19045dac940b4cf8bbe2e16b416
Aslan_Agadadashev <aslan.pressa@yandex.ru>
2023-10-02 17:21:13 +0300

    Add folder/data_create.py

---
+++ folder/data_create.py
@@ -0,0 +1,15 @@
+def input_fname():
+    fnam = str(input("Введите имя: "))
+    return fnam
+
+def input_lname():
+    lnam = str(input("Введите фамилию: "))
+    return lnam
+
+def input_telnum():
+    tel_nam = str(input("Введите номер телефона: "))
+    return tel_nam
+
+def input_address():
+    adr = str(input("Введите адрес: "))
+    return adr
